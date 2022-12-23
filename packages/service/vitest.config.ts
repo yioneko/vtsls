dd51c81e1f85541159b44bee069786c1cc258a1c
yioneko <yioneko@outlook.com>
2022-12-24 00:17:01 +0800

    fix: undefined export of vscode-uri

--- packages/service/vitest.config.ts
+++ packages/service/vitest.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    deps: {
+      // TODO: fix this for vscode-uri
+      interopDefault: true
+    },
+    environment: "node",
     include: ["tests/**/*.{test,spec}.{js,ts}"],
   },
 });
